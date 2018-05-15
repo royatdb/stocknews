68770f10f2b0f8f0a59b8c3c04752f2ea817d098
Debajyoti Roy <roy@databricks.com>
2018-05-15 03:37:27 -0400

    stuff

--- notebooks/Users/debajyoti.roy@databricks.com/Stocks/Model.py
+++ notebooks/Users/debajyoti.roy@databricks.com/Stocks/Model.py
@@ -5,8 +5,9 @@
 
 # COMMAND ----------
 
-from pyspark.ml.classification import DecisionTreeClassifier
+from pyspark.ml.feature import IDF, Tokenizer, RegexTokenizer, StopWordsRemover, CountVectorizer, NGram, VectorAssembler
 from pyspark.ml import Pipeline
+from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
@@ -16,27 +17,35 @@ data = spark.read.table("news_djia")
 
 # COMMAND ----------
 
-splits = data.randomSplit([0.8, 0.2], 42)
+splits = data.randomSplit([0.85, 0.15], 42)
 train = splits[0].cache()
 test = splits[1].cache()
 
 # COMMAND ----------
 
-from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
+tokenizer = [RegexTokenizer().setInputCol("News").setOutputCol("tokens").setPattern("\\W+")]
 
-tokenizer = RegexTokenizer()    \
-  .setInputCol("News")          \
-  .setOutputCol("tokens")       \
-  .setPattern("\\W+")
+remover = [StopWordsRemover().setInputCol("tokens").setOutputCol("stopWordFree")]
+  
+n=2
 
-remover = StopWordsRemover()    \
-  .setInputCol("tokens")        \
-  .setOutputCol("stopWordFree") \
+ngrams = [
+    NGram(n=i, inputCol="stopWordFree", outputCol="{0}_grams".format(i))
+    for i in range(1, n + 1)
+]
 
-counts = CountVectorizer()      \
-  .setInputCol("stopWordFree")  \
-  .setOutputCol("features")     \
-  .setVocabSize(1000)
+cv = [
+    CountVectorizer(vocabSize=1000,inputCol="{0}_grams".format(i),
+        outputCol="{0}_tf".format(i))
+    for i in range(1, n + 1)
+]
+
+idf = [IDF(inputCol="{0}_tf".format(i), outputCol="{0}_tfidf".format(i), minDocFreq=5) for i in range(1, n + 1)]
+
+assembler = [VectorAssembler(
+    inputCols=["{0}_tfidf".format(i) for i in range(1, n + 1)],
+    outputCol="features"
+)]
 
 # COMMAND ----------
 
@@ -46,7 +55,7 @@ evaluator.getMetricName()
 # COMMAND ----------
 
 dtc = DecisionTreeClassifier()
-dtp = Pipeline().setStages([tokenizer, remover, counts, dtc])
+dtp = Pipeline(stages = tokenizer+remover+ngrams+cv+idf+assembler+[dtc] )
 
 # COMMAND ----------
 
@@ -54,10 +63,14 @@ print(dtc.explainParams())
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ![crossValidation](http://curriculum-release.s3-website-us-west-2.amazonaws.com/images/301/CrossValidation.png)
+
+# COMMAND ----------
+
 paramGrid = ParamGridBuilder().\
     addGrid(dtc.maxBins, [32, 64]).\
-    addGrid(dtc.maxDepth, [5, 10]).\
-    addGrid(dtc.impurity, ["gini", "entropy"]).\
+    addGrid(dtc.maxDepth, [5, 30]).\
     build()
     
 crossval = CrossValidator(estimator=dtp,
@@ -79,6 +92,8 @@ print "AUC %(result)s" % {"result": evaluator.evaluate(result)}
 # COMMAND ----------
 
 model_path = "/mnt/roy/stocknews-model"
+dbutils.fs.rm(model_path, True)
+
 model = cvModel.bestModel
 model.write().overwrite().save(model_path)
 
