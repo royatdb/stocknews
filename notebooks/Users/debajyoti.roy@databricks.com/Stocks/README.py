260d58d1eb5b3db0812d6e77d269491e4caa6d72
Debajyoti Roy <roy@databricks.com>
2018-05-15 00:32:56 -0400

    initial

---
+++ notebooks/Users/debajyoti.roy@databricks.com/Stocks/README.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Daily News for Stock Market Prediction
+# MAGIC Using 8 years' daily news headlines to predict stock market movement.
+# MAGIC 
+# MAGIC ![](https://s3.us-east-2.amazonaws.com/databricks-roy/Stocks.jpg)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Logical flow of this Demo:
+# MAGIC ![](https://s3.us-east-2.amazonaws.com/databricks-roy/Data+Flow.jpg)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC #<div style="float:right"><a href="$./Ingest">Ingest</a> <b style="font-size: 160%; color: #1CA0C2;">&#8680;</b></div></div>
+
+# COMMAND ----------
+
